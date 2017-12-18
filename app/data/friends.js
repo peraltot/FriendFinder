eecc32a894bdf738520363a7c2db5aa1acbadc41
peraltot <peraltot@gmail.com>
2017-12-18 15:57:44 -0500

    changed friend array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,14 @@
 var friends = [
     {
-        name: "Mary",
+        name: "Sam",
         photo: "https://c1.staticflickr.com/3/2449/3658822824_35390f1afc_z.jpg?zz=1",
         scores: [1,1,1,1,1,1,1,1,1,1]
     }, {
-        name: "Ruby",
+        name: "Chris",
         photo:"http://4.bp.blogspot.com/-PL-_9kOGEf8/TiAeBWXj6sI/AAAAAAAAAIg/JRJ11NykZls/s1600/south_china_tiger_2.jpg",
         scores: [3,3,3,3,3,3,3,3,3,3]
     }, {
-        name: "Summer",
+        name: "Pat",
         photo: "http://www.funnypica.com/wp-content/uploads/2015/07/Beautiful-Wild-Animal-Photos-22.jpg",
         scores: [5,5,5,5,5,5,5,5,5,5]
     }
